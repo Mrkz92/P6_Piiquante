e20e60c32d3a8d082e020fa1314e4c90307002c5
Mrkz92 <zaorski.marek@gmail.com>
2023-07-12 18:47:04 +0200

    Daily commit 12/07/23

--- controllers/sauceController.js
+++ controllers/sauceController.js
@@ -1,31 +1,45 @@
+const { json } = require("express");
+const { HttpStatus } = require("../managers/httpstatus.js");
 const Sauce = require("../models/sauce");
 
-exports.createSauce = (req, res, next) => {
+exports.createSauce = async (req, res, next) => {
   delete req.body._id;
-  const sauce = new Sauce({
-    userId: req.body.userId,
-    name: req.body.name,
-    manufacturer: req.body.manufacturer,
-    description: req.body.description,
-    mainPepper: req.body.mainPepper,
-    imageUrl: req.body.imageUrl,
-    heat: req.body.heat,
-  });
+  const { userId, name, manufacturer, description, mainPepper, imageUrl, heat } = req.body;
+  const sauce = new Sauce({ userId, name, manufacturer, description, mainPepper, imageUrl, heat });
+  // const sauce = new Sauce({
+  //   userId: req.body.userId,
+  //   name: req.body.name,
+  //   manufacturer: req.body.manufacturer,
+  //   description: req.body.description,
+  //   mainPepper: req.body.mainPepper,
+  //   imageUrl: req.body.imageUrl,
+  //   heat: req.body.heat,
+  // });
   console.log("Nouvelle sauce", sauce.name);
-  sauce
-    .save()
-    .then((sauce) => res.status(201).json({ message: "Nouvelle sauce enregistrée !" }))
-    .catch(sendStatus(400, "Problème survenu"));
+  await sauce.save();
+  throw new HttpStatus(201, { message: "Nouvelle sauce enregistrée !" });
 };
 
-exports.getOneSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then((sauce) => res.status(200).json(sauce))
-    .catch((error) => res.status(404).json({ message: error }));
+exports.modifySauce = async (req, res, next) => {
+  const sauce = await Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id });
+  throw new HttpStatus(200, { message: "La sauce a été modifiée" });
 };
 
-exports.getAllSauces = (req, res, next) => {
-  Sauce.find()
-    .then((sauces) => res.status(200).json(sauces))
-    .catch((error) => res.status(400).json({ message: error }));
+exports.deleteSauce = async (req, res, next) => {
+  const sauce = await Sauce.deleteOne({ _id: req.params.id });
+  throw new HttpStatus(200, { message: "La sauce a bien été supprimée!" });
+};
+
+// exports.likeSauce = async (req, res, next) => {
+//   const sauce = Sauce.updateOne
+// }
+
+exports.getOneSauce = async (req, res, next) => {
+  const sauce = await Sauce.findOne({ _id: req.params.id });
+  throw new HttpStatus(200, json(sauce));
+};
+
+exports.getAllSauces = async (req, res, next) => {
+  const sauces = await Sauce.find();
+  throw new HttpStatus(200, json(sauces));
 };
--- routes/sauceRoute.js
+++ routes/sauceRoute.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const sauceCtrl = require("../controllers/sauceController");
 
-router.get("/", sauceCtrl.getAllSauces);
 router.post("/", sauceCtrl.createSauce);
+router.put("/:id", sauceCtrl.modifySauce);
+router.delete("/:id", sauceCtrl.deleteSauce);
+// router.post("/:id/like", sauceCtrl.likeSauce);
+
+router.get("/", sauceCtrl.getAllSauces);
 router.get("/:id", sauceCtrl.getOneSauce);
 
 module.exports = router;
